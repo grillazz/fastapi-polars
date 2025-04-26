29ebbebc0844af8fc3aa32d08ff44de05c4192a0
grillazz <the@grillazz.com>
2025-04-26 20:36:28 +0200

    bump project deps

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+import logging
+import os
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from api.books import router as grizzly_router
 
-app = FastAPI(title="Grizzly Rest API", version="0.3.0")
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    pid = os.getpid()
+    try:
+        logger.info(f">>> Process ID {pid} saved to file.")
+        yield
+    except Exception as e:
+        logger.error(f"Failed to save process ID to file: {e}")
+        raise
+    finally:
+        # Close any resources here if needed
+        pass
+app = FastAPI(
+    title="Grizzly Rest API",
+    version="0.3.0",
+    lifespan=lifespan,
+    debug=True,
+    contact={
+    "name": "Jakub Miazek",
+    "email": "the@grillazz.com",
+}
+)
 
 app.include_router(grizzly_router, prefix="/grizzly")
